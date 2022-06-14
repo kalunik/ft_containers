956e297c253b42cd17acea3796b7240f85a91d08
kalunik <nikitaex123@gmail.com>
2022-06-14 19:53:36 +0300

    vector.hpp small additions

--- stack.cpp
+++ stack.cpp
@@ -1,6 +1,31 @@
-//
-// Created by Waif Jonathon on 6/3/22.
-//
+#include <memory>
 
-#include "stack.hpp"
+struct A
+{
+	int a;
+	int b;
+	int c;
+};
 
+int main()
+{
+	std::allocator<A> allocator;
+	size_t howMuch = 50;
+	// Выделяем память под 50 структур A. Возвращённый указатель, указывает на неинициализированную память.
+	A* allocatedArray = allocator.allocate(howMuch);
+
+	A* allocatedItem = allocator.allocate(1);
+	// Т.к. память не инициализирована, нам нужно её инициализировать. Для этого вызываем конструктор A явно
+	allocator.construct(allocatedItem);
+	// Для инициализации памяти под массив есть вспомогательная функция, которая сделает то же самое, что
+	// делает allocator по умолчанию
+	std::uninitialized_fill_n(allocatedArray, howMuch, A());
+	// После того, как наигрались с объектом, его нужно удалить, но сначала нужно вызвать деструктор
+	allocator.destroy(allocatedItem);
+	for(size_t i = 0; i < howMuch; ++i)
+		allocator.destroy(allocatedArray + i);
+	// Теперь освобождаем выделенную память
+	allocator.deallocate(allocatedItem, 1);
+	allocator.deallocate(allocatedArray, howMuch);
+	return 0;
+}
--- vector.hpp
+++ vector.hpp
@@ -6,17 +6,33 @@
 #define FT_CONTAINERS_VECTOR_HPP
 
 #include <memory>
-#inlude <stack>
+
 namespace ft{
-	template<class T/*, class Allocator = std::allocator<T>*/>
+	template<class T, class Allocator = std::allocator<T> >
 	class vector {
 	public:
-		typedef T									value_type;
-		typedef std::allocator<T>					allocator_type;
-		typedef typename allocator_type::reference	reference;
-		typedef allocator_type::const_reference		const_reference;
+		typedef T                                        value_type;
+		typedef Allocator                                allocator_type;
+		typedef typename allocator_type::reference       reference;
+		typedef typename allocator_type::const_reference const_reference;
+//		typedef implementation-defined                   iterator;
+//		typedef implementation-defined                   const_iterator;
+		typedef typename allocator_type::size_type       size_type;
+		typedef typename allocator_type::difference_type difference_type;
+		typedef typename allocator_type::pointer         pointer;
+		typedef typename allocator_type::const_pointer   const_pointer;
+//		typedef std::reverse_iterator<iterator>          reverse_iterator;
+//		typedef std::reverse_iterator<const_iterator>    const_reverse_iterator;
 
-		explicit vector(const allocator_type& alloc = allocator_type());
+	protected:
+		T*		_data;
+		size_t	_size;
+		size_t	_capacity;
+
+	public:
+		explicit vector(const allocator_type& alloc = allocator_type()) {
+			reAlloc(2);
+		}
 		explicit vector(size_type n, const value_type& val = value_type(),
 						const allocator_type& alloc = allocator_type());
 		template<class InputIterator>
@@ -26,11 +42,38 @@ namespace ft{
 		~vector();
 		vector& operator= (const vector& x);
 
-		/** ELEMENT ACCESS */
-		reference operator[] (size_type n){
-			return
-		}
+		/* * ELEMENT ACCESS */
+		reference operator[] (size_type n);
 		const_reference operator[] (size_type n) const;
+
+		/* * MODIFIERS */
+		void push_back(const value_type& val) {
+			if (_size >= _capacity)
+				reAlloc(_capacity + _capacity/2);
+
+			_data[_size] = val;
+			_size++;
+		}
+
+		/* * CAPACITY */
+		size_t size() const { return _size; }
+
+	private:
+		/* * UTILS */
+		void reAlloc(size_t newCapacity) {
+			T* newData = new T[newCapacity];
+
+			if (newCapacity < _capacity)
+				_capacity = newCapacity;
+
+			for (size_t i = 0; i < _capacity; i++) {
+				newData[i] = _data[i];
+			}
+
+			delete[] _data;
+			_data = newData;
+			_capacity = newCapacity;
+		}
 	};
 }
 
