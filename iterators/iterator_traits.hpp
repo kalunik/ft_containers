8a183fc30bfe76e9c2afaf27c8bfe5e63cdc5bde
kalunik <nikitaex123@gmail.com>
2022-06-23 01:23:43 +0300

    iterator_traits.hpp added

---
+++ iterators/iterator_traits.hpp
@@ -0,0 +1,37 @@
+//
+// Created by Nikita Kaluzhskikh on 20.06.2022.
+//
+
+#ifndef FT_CONTAINERS_ITERATOR_TRAITS_HPP
+#define FT_CONTAINERS_ITERATOR_TRAITS_HPP
+
+namespace ft {
+	template<typename Iter>
+	struct iterator_traits {
+		typedef typename Iter::difference_type		difference_type;
+		typedef typename Iter::value_type			value_type;
+		typedef typename Iter::pointer				pointer;
+		typedef typename Iter::reference			reference;
+		typedef typename Iter::iterator_category	iterator_category;
+	};
+
+	template<typename T>
+	struct iterator_traits<T*> {
+		typedef std::ptrdiff_t					difference_type;
+		typedef T								value_type;
+		typedef T*								pointer;
+		typedef T&								reference;
+		typedef std::random_access_iterator_tag	iterator_category;
+	};
+
+	template<typename T>
+	struct iterator_traits<const T*> {
+		typedef std::ptrdiff_t					difference_type;
+		typedef T								value_type;
+		typedef const T*						pointer;
+		typedef const T&						reference;
+		typedef std::random_access_iterator_tag	iterator_category;
+	};
+}
+
+#endif //FT_CONTAINERS_ITERATOR_TRAITS_HPP
